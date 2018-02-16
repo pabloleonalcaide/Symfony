27acf7bbea7882ca6111440e540ee97744851526
pabloleonalcaide <pabloleonpsico@gmail.co>
2018-02-16 08:51:01 +0100

    change views

--- ProBlog/src/Blogger/BlogBundle/Controller/DefaultController.php
+++ ProBlog/src/Blogger/BlogBundle/Controller/DefaultController.php
@@ -7,12 +7,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-  //Hemos modificado la anotación de la ruta
-    /**
-     * @Route("/blog")
-     */
-    public function indexAction()
-    {
-        return $this->render('@BloggerBlog/Default/index.html.twig');  //de este modo referenciamos al bundle que hemos creado
-    }
+/**
+* @Route("/blog")
+*/
+public function indexAction()
+{
+return $this->render('@BloggerBlog/index.html.twig');
+}
 }
