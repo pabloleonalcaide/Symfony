5efdeff0897f60ebbb24efae08d018848a1ef095
pabloleonalcaide <pabloleonpsico@gmail.co>
2018-02-16 09:51:32 +0100

    added links

--- ProBlog/src/Blogger/BlogBundle/Controller/page/PageController.php
+++ ProBlog/src/Blogger/BlogBundle/Controller/page/PageController.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Blogger\BlogBundle\Controller;
+namespace Blogger\BlogBundle\Controller\page;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller
+class PageController extends Controller
 {
 /**
 * @Route("/blog", name="index")
@@ -15,10 +15,17 @@ public function indexAction()
 return $this->render('@BloggerBlog/index.html.twig');
 }
 /**
-* @Route("/blog", name="about")
+* @Route("/about", name="about")
 */
 public function aboutAction()
 {
 return $this->render('@BloggerBlog/about.html.twig');
+}
+/**
+* @Route("/contact", name="contact")
+*/
+public function contactAction()
+{
+return $this->render('@BloggerBlog/contact.html.twig');
 }
 }
